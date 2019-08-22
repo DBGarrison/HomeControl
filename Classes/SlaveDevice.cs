059f69c14f3b384eaf267320b61bb0365277d946
DBGarrison <DBGarrison@users.noreply.github.com>
2019-08-22 06:12:51 -0500

    ShopRFRelay added to build list

--- Classes/SlaveDevice.cs
+++ Classes/SlaveDevice.cs
@@ -21,7 +21,28 @@ namespace HomeControl.Classes
         public int port { get; set; }
         public Enums.RemoteOSType osType { get; set; }
 
-        public static List<SlaveDevice>  buildMcuListForTrack()
+      public static List<SlaveDevice>  buildMcuListForTrack(Enums.RemoteOSType osType)
+        {
+            List<SlaveDevice> list = new List<SlaveDevice>();
+            if ((osType & Enums.RemoteOSType.Rpi) == Enums.RemoteOSType.Rpi)
+            {
+             list.Add(new SlaveDevice("RpiZero4", 8000, Enums.RemoteOSType.Rpi));
+             list.Add(new SlaveDevice("RpiZero5", 8000, Enums.RemoteOSType.Rpi));
+             list.Add(new SlaveDevice("RpiZero6", 8000, Enums.RemoteOSType.Rpi));
+             list.Add(new SlaveDevice("RpiZero7", 8000, Enums.RemoteOSType.Rpi));
+             list.Add(new SlaveDevice("RpiZero9", 8000, Enums.RemoteOSType.Rpi));
+             list.Add(new SlaveDevice("RpiZero10", 8000, Enums.RemoteOSType.Rpi));
+             list.Add(new SlaveDevice("RpiZero11", 8000, Enums.RemoteOSType.Rpi));
+            }
+
+            if ((osType & Enums.RemoteOSType.Esp8266) == Enums.RemoteOSType.Esp8266)
+            {
+             list.Add(new SlaveDevice("ShopRfRelay", 80, Enums.RemoteOSType.Esp8266));
+            }
+            return list;
+        }
+     
+        private static List<SlaveDevice>  buildMcuListForTrackOLD()
         {
             List<SlaveDevice> list = new List<SlaveDevice>();
             list.Add(new SlaveDevice("RpiZero4", 8000, Enums.RemoteOSType.Rpi));
